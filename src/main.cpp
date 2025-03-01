4a34919c9549f75b5fc68eda7ed11f6763a123e0
bao.duongngoc <duongngocbao318@gmail.com>
2025-03-01 19:57:30 +0700

    correct variable name in DurationTracker macro

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include "Log.h"
 
 #define DurationTracker(Name) \
-	utils::DurationTracker runAsyncTracker( \
+	utils::DurationTracker LABEL(tracker)( \
 	[](){INFO_LOG("Measure", "{} Begin Tracking elapsed time.", Name);}, \
 	[](utils::DurationTracker::Duration_t i_duration){INFO_LOG("Measure", "{} elapsed time: {}s", Name, std::chrono::duration<double>(i_duration).count());} \
 	);
